755362f4999710a88aab021e024b673887064be3
jw-coder84 <jmw_19@hotmail.com>
2022-08-02 14:44:20 +0000

    Changed add-Game to get_game and completed this function, added validate_data function

--- run.py
+++ run.py
@@ -17,19 +17,55 @@ games = SHEET.worksheet('games')
 data = games.get_all_values()
 
 
-def add_game():
+def get_game():
     """
     Accept a new game from the user to be added to the spreadsheet.
     """
-    genre = ['Adventure', 'Racing', 'FPS', 'RPG', 'Action', 'Platformer', 'Fighting', 'Puzzle', 'Simulation', 'Sports', 'Strategy', 'Visual Novel']
-    print('Please enter the following data separated by comma:')
-    print('Title')
-    print('Genre - options: Adventure, Racing, FPS, RPG, Action, Platformer, \
+
+    while True:
+        print('Please enter the following data separated by comma:')
+        print('Title')
+        print('Genre - options: Adventure, Racing, FPS, RPG, Action, Platformer, \
 Fighting, Puzzle, Simulation, Sports, Strategy, Visual Novel')
-    print('Example: Doom (1993), FPS')
+        print('Platform - options: Nintendo Switch, Playstation, Xbox, Multi-platform, Other')
+        print('Example: Doom (1993), FPS, Multi-platform')
 
-    data_str = input('Enter Title and Genre here:\n')
+        data_str = input('Enter Title, Genre and Platform here:\n')
+        game_data = data_str.split(',')
+        validate_data(game_data)
+        print(f'The data provided is {data_str}')
+        print(game_data)
+
+        if validate_data(game_data):
+            print('Data is valid!')
+            break
+
+    return game_data
 
 
-add_game()
+def validate_data(values):
+    """
+    Raises error if the genre and or platform do not exist in the defined arrays.
+    """
 
+    genre = ['Adventure', 'Racing', 'FPS', 'RPG', 'Action', 'Platformer', 'Fighting', 'Puzzle', 'Simulation', 'Sports', 'Strategy', 'Visual Novel']
+    platform = ['Nintendo Switch', 'Playstation', 'Xbox', 'Multi-platform', 'Other']
+
+    try:
+        if len(values) != 3:
+            raise ValueError(
+                f"Exactly 3 values required, you provided {len(values)}"
+            )
+        elif values[1] not in genre:
+            raise ValueError(f'{values[1]} is not valid genre.')
+
+        elif values[2] not in platform:
+            raise ValueError(f'{values[2]} is not valid platform.')
+    except ValueError as e:
+        print(f"Invalid data: {e}, please try again.\n")
+        return False
+
+    return True
+
+
+get_game()
