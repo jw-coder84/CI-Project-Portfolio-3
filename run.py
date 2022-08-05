5e146d8a0fa730a90bc1bbb7144e6a17dfb34226
jw-coder84 <jmw_19@hotmail.com>
2022-08-05 09:44:07 +0000

    Set votes column value to 0 upon adding game, changed sheet update user messages

--- run.py
+++ run.py
@@ -86,10 +86,11 @@ def update_worksheet(data, worksheet):
     Receives a list of integers to be inserted into a worksheet
     Update the relevant worksheet with the data provided
     """
-    print(f"Updating {worksheet} worksheet...\n")
+    data.append(0)
+    print("Updating games worksheet...\n")
     worksheet_to_update = SHEET.worksheet(worksheet)
     worksheet_to_update.append_row(data)
-    print(f"{worksheet} worksheet updated successfully.\n")
+    print("Your game has been added successfully.\n")
 
 
 def game_vote():
